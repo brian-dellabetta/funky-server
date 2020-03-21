46507f390de83195b5e6bda05049c6cde3c9b8ab
Brian Dellabetta <brian.dellabetta@gmail.com>
2020-03-21 10:49:02 -0500

    initial http4s server

---
+++ src/main/scala/com/brian/funkyserver/Main.scala
@@ -0,0 +1,9 @@
+package com.brian.funkyserver
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    FunkyserverServer.stream[IO].compile.drain.as(ExitCode.Success)
+}
